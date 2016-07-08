8ba8cfdd9375bd108dfb03c44dac07a6d510a47d
djabatav <thomas.craipeau@gmail.com>
2016-07-08 12:26:02 +0400

    update

--- imports/api/server/method.js
+++ imports/api/server/method.js
@@ -17,6 +17,106 @@ import { Events,SchemasEventsRest } from '../events.js'
 import { callPixelRest,authPixelRest } from './api.js';
 
 Meteor.methods({
+  userup: function(geo) {
+    check(geo, {longitude:Number,latitude:Number});
+    if (!this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+    if (Citoyens.update({
+      _id: new Mongo.ObjectID(this.userId)
+    }, {
+      $set: {
+        'geoPosition': {
+          type: "Point",
+          'coordinates': [parseFloat(geo.longitude), parseFloat(geo.latitude)]
+        }
+      }
+    })) {
+      return true;
+    } else {
+      return false;
+    }
+    this.unblock();
+  },
+  'likePhoto': function(photoId) {
+    check(photoId, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    let doc={};
+    doc.id=photoId;
+    doc.collection="news";
+    doc.action="voteUp";
+    let voteQuery={};
+    voteQuery["_id"] = new Mongo.ObjectID(photoId);
+    voteQuery['voteUp.'+this.userId]={$exists:true};
+    console.log(voteQuery);
+
+console.log(JSON.stringify(News.findOne(voteQuery)));
+
+    if (News.findOne(voteQuery)) {
+      doc.unset="true";
+      Meteor.call('addAction',doc);
+
+    } else {
+      let voteQuery={};
+      voteQuery["_id"] = new Mongo.ObjectID(photoId);
+      voteQuery['voteDown.'+this.userId]={$exists:true};
+      console.log(voteQuery);
+
+      if (News.findOne(voteQuery)) {
+        let rem={};
+        rem.id=photoId;
+        rem.collection="news";
+        rem.action="voteDown";
+        rem.unset="true";
+        Meteor.call('addAction',rem);
+
+      }
+      Meteor.call('addAction',doc);
+
+    }
+  },
+  'dislikePhoto': function(photoId) {
+    check(photoId, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    let doc={};
+    doc.id=photoId;
+    doc.collection="news";
+    doc.action="voteDown";
+    let voteQuery={};
+    voteQuery["_id"] = new Mongo.ObjectID(photoId);
+    voteQuery['voteDown.'+this.userId]={$exists:true};
+    console.log(voteQuery);
+
+    if (News.findOne(voteQuery)) {
+      doc.unset="true";
+      Meteor.call('addAction',doc);
+    } else {
+
+      let voteQuery={};
+      voteQuery["_id"] = new Mongo.ObjectID(photoId);
+      voteQuery['voteUp.'+this.userId]={$exists:true};
+      console.log(voteQuery);
+
+      if (News.findOne(voteQuery)) {
+        let rem={};
+        rem.id=photoId;
+        rem.collection="news";
+        rem.action="voteUp";
+        rem.unset="true";
+        Meteor.call('addAction',rem);
+      }
+      Meteor.call('addAction',doc);
+
+    }
+  },
   surveyAdAction : function(doc){
     check(doc.id, String);
     check(doc.collection, String);
