ab4e3a7727e1bf7bfb4c026260f454cd6775a229
aboire <thomas.craipeau@gmail.com>
2016-08-31 12:34:38 +0400

    update login method

--- imports/api/server/config.js
+++ imports/api/server/config.js
@@ -8,40 +8,58 @@ Accounts.registerLoginHandler(function(loginRequest) {
   if(!loginRequest.email || !loginRequest.pwd) {
     return null;
   }
-  var pswdDigest = SHA256(loginRequest.email+loginRequest.pwd)
-  //var pswdDigest = CryptoJS.SHA256(loginRequest.email+loginRequest.pwd).toString();
 
-  var userId = null;
-  var userC = Citoyens.findOne({email: loginRequest.email,pwd:pswdDigest},{fields:{pwd:0}});
+    const response = HTTP.call( 'POST', Meteor.settings.endpoint+'/communecter/person/authenticate', {
+      params: {
+        "email": loginRequest.email,
+        "pwd": loginRequest.pwd,
+      }
+    });
+    //console.log(response);
+    if(response && response.data && response.data.result && response.data.id){
+      //var pswdDigest = SHA256(loginRequest.email+loginRequest.pwd)
+      //var pswdDigest = CryptoJS.SHA256(loginRequest.email+loginRequest.pwd).toString();
+      var userId = null;
+      //var userC = Citoyens.findOne({email: loginRequest.email,pwd:pswdDigest},{fields:{pwd:0}});
 
-  if(!userC) {
-    throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Communecter Login Failed');;
-  } else {
-    //ok valide
-    var userM = Meteor.users.findOne({'_id':userC._id._str});
-    console.log(userM);
-    if(userM && userM.profile &&  userM.profile.pixelhumain){
-      //Meteor.user existe
-      userId= userM._id;
-      Meteor.users.update(userId,{$set: {'profile.pixelhumain': userC,emails:[userC.email]}});
+      var userC = Citoyens.findOne({ _id: new Mongo.ObjectID(response.data.id.$id) },{fields:{pwd:0}});
+
+      if(!userC) {
+        throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Communecter Login Failed');
+      } else {
+        //ok valide
+        var userM = Meteor.users.findOne({'_id':userC._id._str});
+        console.log(userM);
+        if(userM && userM.profile &&  userM.profile.pixelhumain){
+          //Meteor.user existe
+          userId= userM._id;
+          Meteor.users.update(userId,{$set: {'profile.pixelhumain': userC,emails:[userC.email]}});
+        }else{
+          //Meteor.user n'existe pas
+          //username ou emails
+          userId = Meteor.users.insert({_id:userC._id._str,emails:[userC.email]});
+          Meteor.users.update(userId,{$set: {'profile.pixelhumain': userC}});
+        }
+      }
+
+      var stampedToken = Accounts._generateStampedLoginToken();
+      Meteor.users.update(userId,
+        {$push: {'services.resume.loginTokens': stampedToken}}
+      );
+      this.setUserId(userId);
+      console.log(userId);
+      return {
+        userId: userId,
+        token: stampedToken.token
+      }
     }else{
-      //Meteor.user n'existe pas
-      //username ou emails
-      userId = Meteor.users.insert({_id:userC._id._str,emails:[userC.email]});
-      Meteor.users.update(userId,{$set: {'profile.pixelhumain': userC}});
-    }
-  }
+      if(response && response.data && response.data.result === false){
+        throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Communecter Login Failed');
+      } else if(response && response.data && response.data.result === true && response.data.msg){
+        throw new Meteor.Error(response.data.msg);
+      }
 
-  var stampedToken = Accounts._generateStampedLoginToken();
-  Meteor.users.update(userId,
-    {$push: {'services.resume.loginTokens': stampedToken}}
-  );
-  this.setUserId(userId);
-  console.log(userId);
-  return {
-    userId: userId,
-    token: stampedToken.token
-  }
+    }
 });
 
 Push.debug = true;
