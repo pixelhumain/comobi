549b5d7d0c6eddfd150f89daf1206face21b958e
djabatav <thomas.craipeau@gmail.com>
2017-05-29 16:34:42 +0400

    update

--- mobile-config.js
+++ mobile-config.js
@@ -4,8 +4,8 @@ App.info({
   description: 'communecter mobile',
   author: 'thomas',
   email: 'thomas.craipeau@gmail.com',
-  version: '0.0.2',
-  buildNumber: '102'
+  version: '0.0.3',
+  buildNumber: '103'
 });
 
 App.setPreference('android-targetSdkVersion', '23');
