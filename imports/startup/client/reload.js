1949902d0a45e1d6b61e54be02523dd00fc29ea4
aboire <thomas.craipeau@gmail.com>
2019-01-16 22:52:03 +0400

    update reload

--- imports/startup/client/reload.js
+++ imports/startup/client/reload.js
@@ -1,11 +1,27 @@
-import { Reload } from 'meteor/reload';
-import { Tracker } from 'meteor/tracker';
-import { TAPi18n } from 'meteor/tap:i18n';
-import { IonPopup } from 'meteor/meteoric:ionic';
 
-Tracker.autorun(() => {
+import { Meteor } from 'meteor/meteor';
+import { Reloader } from 'meteor/jamielob:reloader';
+
+/* Meteor._reload.onMigrate(function() {
+return [false];
+}); */
+
+/* Tracker.autorun((c) => {
   if (Reload.isWaitingForResume()) {
-    // window.location.replace(window.location.href)
-    IonPopup.alert({ template: TAPi18n.__('Close and reopen the application to get the new version') });
+    IonPopup.alert({ template: TAPi18n.__('Close and reopen the application to get the new version!') });
+    c.stop();
   }
-});
+}); */
+
+if (Meteor.isDevelopment) {
+  Reloader.configure({
+    check: false, // don't check on startup
+    refresh: 'instantly', // refresh as soon as updates are available
+  });
+} else {
+  Reloader.configure({
+    check: 'firstStart', // Only make an additonal check the first time the app ever starts
+    checkTimer: 5000, // Wait 5 seconds to see if new code is available on first start
+    refresh: 'start', // Only refresh to already downloaded code on a start and not a resume
+  });
+}
